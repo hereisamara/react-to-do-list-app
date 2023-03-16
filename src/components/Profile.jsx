c844b2d35f2c09afb04a500dd0476395cf402a34
Khin Eaindray Htun <91376371+hereisamara@users.noreply.github.com>
2023-03-16 14:00:45 +0700

    react todolist app

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+
+function Profile(){
+    return (
+        <div>
+            <img src="assets/croppic.png" alt=""/>
+            <h1>Amara</h1>
+        </div>
+    )
+}
+
+export default Profile;
